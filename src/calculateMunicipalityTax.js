bff5f8c09ead073a63540122fef711768c25c73a
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2018-06-18 15:09:29 +0300

    Implemented basic municipalitytax module

---
+++ src/calculateMunicipalityTax.js
@@ -0,0 +1,107 @@
+import { calculateIncomeTax } from "./calculateIncomeTax";
+import config from "./config";
+
+function deductIncomeGenerationDeduction(salary) {
+  const maxIncomeGenerationDeduction = 750;
+  return (
+    salary -
+    (salary > maxIncomeGenerationDeduction
+      ? maxIncomeGenerationDeduction
+      : salary)
+  );
+}
+
+function deductPensionInsurance(effectiveIncome, salary) {
+  const pensionInsurancePercentage = 0.0615; // 6.15 %
+  return effectiveIncome - salary * pensionInsurancePercentage;
+}
+
+function deductUnemploymentInsurance(effectiveIncome, salary) {
+  const unemploymentInsurancePercentage = 0.016; // 1.6 %
+  return effectiveIncome - salary * unemploymentInsurancePercentage;
+}
+
+function deductHealthInsurance(effectiveIncome, salary) {
+  const healthInsurancePercentage = 0.0158; // 1.58%
+  if (salary > 14000) {
+    return effectiveIncome - salary * healthInsurancePercentage;
+  }
+  return effectiveIncome;
+}
+
+function calculateEffectiveIncome(salary) {
+  // Tulonhankkimisvähennys
+  let effectiveIncome = deductIncomeGenerationDeduction(salary);
+
+  // Työeläkevakuutusmaksu
+  effectiveIncome = deductPensionInsurance(effectiveIncome, salary);
+
+  // Työttömyysvakuutusmaksu
+  effectiveIncome = deductUnemploymentInsurance(effectiveIncome, salary);
+
+  // Sairausvakuutuksen päivärahamaksu
+  return deductHealthInsurance(effectiveIncome, salary);
+}
+
+function calculateIncomeDeduction(salary) {
+  let incomeDeduction = (7230 - 2500) * 0.51 + (salary - 7230) * 0.28;
+  incomeDeduction = incomeDeduction > 3570 ? 3570 : incomeDeduction;
+  incomeDeduction = incomeDeduction - (salary - 750 - 14000) * 0.045;
+  return incomeDeduction < 0 ? 0 : incomeDeduction;
+}
+
+function calculateBasicDeduction(effectiveIncome, salary) {
+  const incomeDeduction = calculateIncomeDeduction(salary);
+  const basicDeduction =
+    3060 - (effectiveIncome - incomeDeduction - 3060) * 0.18;
+  return basicDeduction < 0 ? 0 : basicDeduction;
+}
+
+function maxEarningDeduction(effectiveIncome, salary) {
+  if (salary > 33000) {
+    const stateEffectiveIncome = (effectiveIncome - 2500) * 0.12;
+    const stateEffectiveIncomeMax =
+      stateEffectiveIncome > 1420 ? 1420 : stateEffectiveIncome;
+    const overEffectiveStateIncomeMax = salary - 750 - 33000;
+    return stateEffectiveIncomeMax - overEffectiveStateIncomeMax * 0.0151;
+  }
+  return 1420;
+}
+
+function calculateEarningsDeduction(effectiveIncome, salary, stateIncomeTax) {
+  const maxEarningsDeduction = maxEarningDeduction(effectiveIncome, salary);
+  const workIncomeDeduction = maxEarningsDeduction - stateIncomeTax;
+  return workIncomeDeduction < 0 ? 0 : workIncomeDeduction;
+}
+
+export function calculateMunicipalityTax(salary) {
+  // Laskennalliset vuositulot
+  const effectiveIncome = calculateEffectiveIncome(salary);
+
+  // Valtion verot
+  const stateIncomeTax = calculateIncomeTax(salary);
+
+  // Ansiotulovähennys
+  const incomeDeduction = calculateIncomeDeduction(salary);
+
+  // Perusvähennys
+  const basicDeduction = calculateBasicDeduction(effectiveIncome, salary);
+
+  // Työtulovähennys
+  const earningDeduction = calculateEarningsDeduction(
+    effectiveIncome,
+    salary,
+    stateIncomeTax
+  );
+
+  const effectiveIncomeAfterDeductions =
+    effectiveIncome - incomeDeduction - basicDeduction;
+
+  return (
+    Math.round(
+      (effectiveIncomeAfterDeductions * config.municipality_tax.average -
+        earningDeduction) *
+        100
+    ) / 100
+  );
+}
--- src/config.js
+++ src/config.js
@@ -4,5 +4,8 @@ export default {
     level2: { limit: 25300, amount: 533, tax: 0.175 },
     level3: { limit: 41200, amount: 3315.5, tax: 0.215 },
     level4: { limit: 73100, amount: 10174, tax: 0.315 }
+  },
+  municipality_tax: {
+    average: 0.1986
   }
 };
